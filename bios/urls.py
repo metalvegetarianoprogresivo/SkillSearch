ec5e3ba8d208d9042cf4802ac7344b4858453965
AlanMunozN <amunoz@intersysconsulting.com>
2018-09-11 15:09:02 -0500

    fixed views

--- bios/urls.py
+++ bios/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='bios_index'),
     path('get_documents/', views.get_documents, name='get_documents'),
+    path('send_logs/', views.send_logs, name='send_logs'),
 ]
