22b49e5a0ad036deccae756bdd3836c7950266e7
MMSaavedraC <msaavedra@intersysconsulting.com>
2018-12-06 13:05:55 -0600

    bug fixed

--- consultantmarket/redirect_url.py
+++ consultantmarket/redirect_url.py
@@ -1,9 +1,15 @@
+from django.contrib.auth.models import User
+
 def redirect_url(request):
     main_domain = read_main_url()
-    if(request.session["authenticated"] == None or request.session["authenticated"] == False):
-        return main_domain[0]
-    return False
+    if(request.session.get("authenticated", None) == None or request.session.get("authenticated", False) == False or request.session.get('mail', None) == None):
+        return main_domain
+    else:
+        intersys_user = User.objects.get(email = request.session['mail'])
+        request.user = intersys_user 
+        return False
 
 def read_main_url():
     file = open("properties.txt","r").read()
+    print("leyo"+file.splitlines()[0])
     return file.splitlines()[0]
